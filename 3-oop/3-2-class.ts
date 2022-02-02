177cdd3a59c31c9cc68d37357c42782d1ba2f28d
seanstory-dev <seanstory.dev@gmail.com>
2022-02-02 20:19:02 +0900

    Add coffee machine with oop

---
+++ 3-oop/3-2-class.ts
@@ -0,0 +1,38 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  class CoffeeMaker {
+    static BEANS_GRAM_PER_SHOT: number = 8; // class level
+    coffeeBeans: number = 0; // instance(object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMaker {
+      return new CoffeeMaker(coffeeBeans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAM_PER_SHOT;
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+  }
+
+  const maker = new CoffeeMaker(20);
+  console.log(maker);
+  console.log(maker.makeCoffee(2));
+
+  const maker2 = CoffeeMaker.makeMachine(20);
+  console.log(maker2);
+  console.log(maker2.makeCoffee(2));
+}
